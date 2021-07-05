137ada7e9fe44069d02faea7320094626cf2642f
Lemelkya <lemelkya@outlook.com>
2021-07-05 10:31:54 +0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-require('./funcs/generateId')
-
-module.exports = (mongoUrl, token) => {
-
-    const crystalrun = require("./src/startService");
-    if(!mongoUrl) return console.log('[ CRYSTALMONGOD ] Please provide a mongodb url.')
-    crystalrun(mongoUrl, token)
-  };
-
