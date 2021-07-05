275ce31500d054b6195ffba53e344e147183434e
Lemelkya <lemelkya@outlook.com>
2021-07-05 10:31:41 +0300

    Delete src directory

--- src/startService.js
+++
@@ -1,8 +0,0 @@
-const cacheManager = require("./mongod/cache")
-
-module.exports = (mongoUrl, token) => {    
-    console.log(`[ CRYSTALMONGOD ] Initialized..`)
-    if(!mongoUrl) return console.log('[ CRYSTALMONGOD ] Please provide a mongodb url.')
-    cacheManager(mongoUrl, token)
-    setTimeout(() => console.log(`[ CRYSTALMONGOD ] Error! DB url is not valid..`), 5000);
-  };
